a22519246193bf2760d69e58063606a545d3d981
Svante Magnell <44971575+manell0@users.noreply.github.com>
2021-05-04 11:59:39 +0200

    Add a sendEmail.js file for sending email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+	emailjs
+		.send('service_h2aehq8', 'template_e484apl', {
+			from_name: contactForm.name.value,
+			from_email: contactForm.emailaddress.value,
+			project_request: contactForm.projectsummary.value,
+		})
+		.then(
+			function (response) {
+				console.log('SUCCESS', response);
+			},
+			function (error) {
+				console.log('FAILED', error);
+			}
+		);
+	return false; // To block from loading a new page
+}
